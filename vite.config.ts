e816af7a233a6a39ac3f15389cbd2ec7cafe4b45
Pier Fumagalli <pier@usrz.com>
2022-07-27 16:28:20 +0200

    Add support for ESLint

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
+import { viteSingleFile } from 'vite-plugin-singlefile'
+
+import viteEslint from 'vite-plugin-eslint'
 import vue from '@vitejs/plugin-vue'
-import { viteSingleFile } from "vite-plugin-singlefile"
 
 export default defineConfig({
-  plugins: [ vue(), viteSingleFile() ],
+  plugins: [ vue(), viteEslint(), viteSingleFile() ],
 })
