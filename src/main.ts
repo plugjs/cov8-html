718388765e91c963310ec5da8cc886e45617b8a1
Pier Fumagalli <pier@usrz.com>
2022-07-27 19:02:07 +0200

    Comments

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from '@vue/runtime-dom'
 import App from './app.vue'
 
+/* Import PrismJS styles and scripts, they'll hook to window.Prism */
 import 'prismjs/themes/prism.css'
 import 'prismjs/plugins/line-numbers/prism-line-numbers.css'
 
@@ -11,13 +12,15 @@ import 'prismjs/components/prism-typescript'
 import 'prismjs/plugins/line-numbers/prism-line-numbers'
 import 'prismjs/plugins/keep-markup/prism-keep-markup'
 
+/* Our CSS */
 import './main.css'
 
+/* Callback for our JSONP-formatted report */
 window.__initCoverage__ = function(report: CoverageReport) {
   createApp(App, { report }).mount('#app')
 }
 
-/** Hack to load our  */
+/** Hack to load our JSONO-formatted report (we use files...) */
 document.addEventListener('DOMContentLoaded', () => {
   const coverageScript = document.createElement('script')
   coverageScript.setAttribute('src', 'report.js')
