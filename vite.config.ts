4cebd2f16af5019afff904e9afba43dfffb2f040
Pier Fumagalli <pier@usrz.com>
2024-07-19 11:49:55 +0200

    Disable linting while building (we lint right before calling Vite)

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import { viteSingleFile } from 'vite-plugin-singlefile'
-// @ts-expect-error // vite-plugin-eslint has no types
-import viteEslint from 'vite-plugin-eslint'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [ vue(), viteEslint(), viteSingleFile() ],
+  plugins: [ vue(), viteSingleFile() ],
 })
