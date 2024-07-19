0246f584f945766a1afeb41b6e0599b66336b078
Pier Fumagalli <pier@usrz.com>
2024-07-19 11:40:38 +0200

    Move to new ESLint

---
+++ eslint.config.mjs
@@ -0,0 +1,79 @@
+import configurations from '@plugjs/eslint-plugin'
+import vuePlugin from 'eslint-plugin-vue'
+import vueParser from 'vue-eslint-parser'
+import * as espree from 'espree'
+
+
+export default [
+  // ===== GENERAL JS/TS CONFIGURATIONS FROM PLUGJS AND VUE ====================
+
+  // Any config for TS also applies to VUE files
+  ...configurations.map((config) => {
+    if (config.files?.includes('**/*.ts')) config.files.push('**/*.vue')
+    return config
+  }),
+
+  // All VUE configurations are restricted to ".vue" files
+  ...vuePlugin.configs['flat/recommended'].map((config) => {
+    config.files = [ '**/*.vue' ]
+    return config
+  }),
+
+  // ===== PARSERS CONFIGURATION FOR VUE ========================================
+
+  // Set the *default* parser, for all files, to be specifically the VUE parser
+  // backed by TypeScript ESLint's own parser (all our .vue are in TypeScript)
+  {
+    name: 'local/parser/vue',
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        extraFileExtensions: [ '.vue' ],
+        parser: '@typescript-eslint/parser',
+        project: [
+          './tsconfig.app.json',
+          './tsconfig.node.json',
+        ],
+      },
+    },
+  },
+
+  // Specifically for JavaScript files, use the default "espree" parser, as
+  // JavaScript sources (like this file) are not present in "tsconfig.json".
+  {
+    name: 'local/parser/js',
+    files: [ '**/*.mjs', '**/*.cjs', '**/*.js' ],
+    languageOptions: {
+      parser: espree,
+    },
+  },
+
+  // ===== PROJECT LOCAL CONFIGURATIONS ========================================
+
+  {
+    name: 'local/rules/vue',
+    files: [ '**/*.vue' ],
+    rules: {
+      // We don't care about "multiple words" in components
+      'vue/multi-word-component-names': [ 'off' ],
+      // Either 3 attributes on a single line, or all attributes on newlines!
+      'vue/max-attributes-per-line': [ 'warn', {
+        'singleline': 3,
+      } ],
+      // No unused variables, ever
+      'vue/no-unused-vars': 'error',
+    },
+  },
+
+  // ===== IGNORED FILES =======================================================
+  // REMEMBER! Ignores *must* be in its own configuration, they can not coexist
+  // with "rules", "languageOptions", "files", ... or anything else, otherwise
+  // ESLint will blaantly ignore the ignore files!
+  {
+    name: 'local/ignores',
+    ignores: [
+      'dist/',
+      'public/',
+    ],
+  },
+]
