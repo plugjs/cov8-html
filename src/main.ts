188e5aa1121125ecab97171d7b7de50a7675335c
Pier Fumagalli <pier@usrz.com>
2024-09-11 20:28:48 +0200

    Make sure icon is embedded in the (only) source HTML

--- src/main.ts
+++ src/main.ts
@@ -12,15 +12,21 @@ import { createApp } from '@vue/runtime-dom'
 /* Our CSS and app */
 import './main.css'
 import App from './app.vue'
+import IconDataUrl from '../resources/v8.png?url'
 
 /* Callback for our JSONP-formatted report */
 window.__initCoverage__ = function(report: CoverageReport) {
   createApp(App, { report }).mount('#app')
-  // window.__coverageData__ = report
 }
 
 /** Hack to load our JSONO-formatted report (we use files...) */
 document.addEventListener('DOMContentLoaded', () => {
+  const link = document.createElement('link')
+  link.setAttribute('rel', 'icon')
+  link.setAttribute('type', 'image/png')
+  link.setAttribute('href', IconDataUrl)
+  document.head.appendChild(link)
+
   const coverageScript = document.createElement('script')
   coverageScript.setAttribute('src', 'report.js')
   document.body.appendChild(coverageScript)
