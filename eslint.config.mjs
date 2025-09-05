823c74b34908e4e84187e35279d4142adc508e48
Pier Fumagalli <pier@usrz.com>
2025-09-05 20:03:28 +0200

    Shrink ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,70 +1,7 @@
-import configurations from '@plugjs/eslint-plugin'
-import vuePlugin from 'eslint-plugin-vue'
-import vueParser from 'vue-eslint-parser'
-import * as espree from 'espree'
-
+import configurations from '@plugjs/eslint-plugin-vue'
 
 export default [
-  // ===== GENERAL JS/TS CONFIGURATIONS FROM PLUGJS AND VUE ====================
-
-  // Any config for TS also applies to VUE files
-  ...configurations.map((config) => {
-    if (config.files?.includes('**/*.ts')) config.files.push('**/*.vue')
-    return config
-  }),
-
-  // All VUE configurations are restricted to ".vue" files
-  ...vuePlugin.configs['flat/recommended'].map((config) => {
-    config.files = [ '**/*.vue' ]
-    return config
-  }),
-
-  // ===== PARSERS CONFIGURATION FOR VUE ========================================
-
-  // Set the *default* parser, for all files, to be specifically the VUE parser
-  // backed by TypeScript ESLint's own parser (all our .vue are in TypeScript)
-  {
-    name: 'local/parser/vue',
-    languageOptions: {
-      parser: vueParser,
-      parserOptions: {
-        extraFileExtensions: [ '.vue' ],
-        parser: '@typescript-eslint/parser',
-      },
-    },
-
-    rules: {
-      // This requires "projects", and "projects" doubles the linting time
-      '@typescript-eslint/no-floating-promises': [ 'off' ],
-    },
-  },
-
-  // Specifically for JavaScript files, use the default "espree" parser, as
-  // JavaScript sources (like this file) are not present in "tsconfig.json".
-  {
-    name: 'local/parser/js',
-    files: [ '**/*.mjs', '**/*.cjs', '**/*.js' ],
-    languageOptions: {
-      parser: espree,
-    },
-  },
-
-  // ===== PROJECT LOCAL CONFIGURATIONS ========================================
-
-  {
-    name: 'local/rules/vue',
-    files: [ '**/*.vue' ],
-    rules: {
-      // We don't care about "multiple words" in components
-      'vue/multi-word-component-names': [ 'off' ],
-      // Either 3 attributes on a single line, or all attributes on newlines!
-      'vue/max-attributes-per-line': [ 'warn', {
-        'singleline': 3,
-      } ],
-      // No unused variables, ever
-      'vue/no-unused-vars': 'error',
-    },
-  },
+  ...configurations,
 
   // ===== IGNORED FILES =======================================================
   // REMEMBER! Ignores *must* be in its own configuration, they can not coexist
